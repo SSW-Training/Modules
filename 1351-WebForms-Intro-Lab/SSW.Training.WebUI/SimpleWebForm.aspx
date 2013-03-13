f49f2a5acf6c3ba9f9d5c815aa0ffb2698da335d
Adam Stephensen <github@adamstephensen.com>
2013-03-13 12:18:31 +1000

    1351-WebForms-Intro-Lab: Add Simple WebForm

---
+++ 1351-WebForms-Intro-Lab/SSW.Training.WebUI/SimpleWebForm.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SimpleWebForm.aspx.cs" Inherits="SSW.Training.WebUI.SimpleWebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="TestButton" runat="server" Text="Test" OnClick="TestButton_Click" />
+        <asp:Label ID="ResultLabel" runat="server" Text="Result"></asp:Label>  
+    </div>
+    </form>
+</body>
+</html>
---
+++ 1351-WebForms-Intro-Lab/SSW.Training.WebUI/SimpleWebForm.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SSW.Training.WebUI
+{
+    public partial class SimpleWebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ResultLabel.Text = "Page_Load";
+        }
+
+        protected void TestButton_Click(object sender, EventArgs e)
+        {
+            ResultLabel.Text = DateTime.Now.ToString();
+        }
+    }
+}
