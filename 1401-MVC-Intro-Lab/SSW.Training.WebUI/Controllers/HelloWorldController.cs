93f2b5da21b49ea6f41d9665a459c83b5cce8703
Adam Stephensen <github@adamstephensen.com>
2013-03-25 21:03:56 +1000

    1401-MVC-Intro-Lab: Add Hello World Controller

---
+++ 1401-MVC-Intro-Lab/SSW.Training.WebUI/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SSW.Training.WebUI.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is the default action method";
+        }
+
+        public string Welcome(string target)
+        {
+            return HttpUtility.HtmlEncode(string.Format("Hello {0} !" ,target));
+        } 
+    }
+}
+
+
