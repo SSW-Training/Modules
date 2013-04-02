6fb73a4957db4de5b21443cedff74c9f3a858748
Adam Stephensen <github@adamstephensen.com>
2013-04-02 22:59:14 +1000

    1402-MVC-Data-Scaffolding-Lab

---
+++ 1402-MVC-Data-Scaffolding-Lab/SSW.Training.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SSW.Training.WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        //
+        // GET: /Products/
+
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.Category).Include(p => p.Supplier);
+            return View(products.ToList());
+        }
+
+        //
+        // GET: /Products/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        //
+        // GET: /Products/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName");
+            return View();
+        }
+
+        //
+        // POST: /Products/Create
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", product.SupplierID);
+            return View(product);
+        }
+
+        //
+        // GET: /Products/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", product.SupplierID);
+            return View(product);
+        }
+
+        //
+        // POST: /Products/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", product.SupplierID);
+            return View(product);
+        }
+
+        //
+        // GET: /Products/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        //
+        // POST: /Products/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Search(string id)
+        {
+
+            var searchText = id;
+
+            var products =
+                db.Products.Include(p => p.Category)
+                  .Include(p => p.Supplier)
+                  .Where(p => p.ProductName.Contains(searchText));
+            return View(products.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
