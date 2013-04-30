3eb577ebba29175dcd5cf728a0bf87f17f11d41f
Adam Stephensen <github@adamstephensen.com>
2013-04-30 11:01:58 +1000

    1801-Unit-Test-Intro-Lab
    ready for class

---
+++ 1801-Unit-Test-Intro-Lab/SSW.Training.Business.Tests.Unit/StringValidationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SSW.Training.Business.Tests.Unit
+{
+    public static class StringValidationExtensions
+    {
+        public static bool IsValidEmailAddress(this string email)
+        {
+            Regex regex = new Regex(@"^[\w-]+(?:\.[\w-]+)*@(?:[\w-]+\.)+[a-zA-Z]{2,7}$");
+            Match match = regex.Match(email);
+            return match.Success;
+        }
+    }
+}
---
+++ 1801-Unit-Test-Intro-Lab/SSW.Training.Business.Tests.Unit/StringValidationExtensionsTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SSW.Training.Business.Tests.Unit
+{
+    [TestClass]
+    public class StringValidationExtensionsTests
+    {
+        [TestMethod]
+        public void IsValidEmailAddress_ForValidEmail_ReturnTrue()
+        {
+            //arrange
+            var stringToTest = "adam@stephensen.me";
+
+            //act
+            var result = stringToTest.IsValidEmailAddress();
+
+            //assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsValidEmailAddress_ForInValidEmailContainingSpaces_ReturnFalse()
+        {
+            //arrange
+            var stringToTest = "adam@stephensen .me";
+
+            //act
+            var result = stringToTest.IsValidEmailAddress();
+
+            //assert
+            Assert.IsFalse(result);
+
+        }
+
+        [TestMethod]
+        public void IsValidEmailAddress_ForInValidEmailContainingNoAt_ReturnFalse()
+        {
+            //arrange
+            var stringToTest = "adamstephensen.me";
+
+            //act
+            var result = stringToTest.IsValidEmailAddress();
+
+            //assert
+            Assert.IsFalse(result);
+
+        }
+    }
+
+}
